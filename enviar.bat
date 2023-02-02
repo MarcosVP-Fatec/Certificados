33f4024b9ab76db557b5f5aa94226f16c2539411
MarcosVP <marcos.pereira47@fatec.sp.gov.br>
2023-02-02 15:37:43 -0300

    AtualizaþÒo

--- enviar.bat
+++ enviar.bat
@@ -1,6 +1,17 @@
 @echo on
+@prompt $G$S
+
 git status
+echo %errorlevel%
+
+@choice /C SN /M "Subir atualização" /T 2 /D N
+echo %errorlevel%
+@if errorlevel 2 goto fim
+
 git add .
 git commit -m "Atualização"
 git push
 git status
+
+:fim
+@@prompt $P$G$S
